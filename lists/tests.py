52c090dc687cb9b7cdab827b3ac84afa283ca78d
ratskevichdg <ratskevichdg@gmail.com>
2021-07-02 12:05:08 +0300

    First unit test and url conversion, dummy representation

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-    """Smoke test"""
+class HomePageTest(TestCase):
+    """Home page test"""
 
-    def test_bad_math(self):
-        """test: incorrect math calculations"""
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """test: root urls resolves to home page view"""
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
